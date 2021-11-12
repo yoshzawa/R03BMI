82fa11210599701a4ae53104428d755c7523d94c
yoshzawa <yoshzawa@gmail.com>
2021-11-12 10:29:56 +0900

    プロジェクト ファイルを追加します。

---
+++ R03BMI/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace R03BMI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            creater.Text = "JK3A00 吉澤　毅";
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
